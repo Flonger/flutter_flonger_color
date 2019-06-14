87f1a1274fe1a209a1c39924ad0238dd0ffc4572
Flonger <xue@flonger.com>
2019-06-14 15:08:08 +0800

    0.0.1

---
+++ test/flutter_flonger_color_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_flonger_color/flutter_flonger_color.dart';
+
+void main() {
+  test('adds one to input values', () {
+  });
+}
